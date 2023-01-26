a8b1f26fb413845ef8138a7b4f9f49b1307312ff
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-01-26 14:22:42 +0000

    started week 2

---
+++ Week2/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            int firstNum = Convert.ToInt32(TxtBoxOne.Text), secondNum = Convert.ToInt32(TxtBoxTwo.Text);
+
+            lblAnswer.Content = $"{firstNum} + {secondNum} = {firstNum + secondNum}";
+        }
+
+        private void BtnDevid_Click(object sender, RoutedEventArgs e)
+        {
+            int firstNum = Convert.ToInt32(TxtBoxOne.Text), secondNum = Convert.ToInt32(TxtBoxTwo.Text);
+
+            lblAnswer.Content = $"{firstNum} / {secondNum} = {firstNum / secondNum}";
+        }
+
+        private void BtnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            int firstNum = Convert.ToInt32(TxtBoxOne.Text), secondNum = Convert.ToInt32(TxtBoxTwo.Text);
+
+            lblAnswer.Content = $"{firstNum} - {secondNum} = {firstNum - secondNum}";
+        }
+
+        private void BtnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            int firstNum = Convert.ToInt32(TxtBoxOne.Text), secondNum = Convert.ToInt32(TxtBoxTwo.Text);
+
+            lblAnswer.Content = $"{firstNum} * {secondNum} = {firstNum * secondNum}";
+        }
+
+    }
+}
